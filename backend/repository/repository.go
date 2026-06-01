cd60183a8044ba239f621353a20e1bd5dc89b4d2
Keshav Bansal <keshavbansal9a@gmail.com>
2026-06-02 01:50:12 +0530

    Added the repo function for registering a new user

--- backend/repository/repository.go
+++ backend/repository/repository.go
@@ -1,7 +1,39 @@
 package repository
 
-import "github.com/KeshavBansal42/Lords_of_Larceny/backend/dtos"
+import (
+	"context"
+	"errors"
 
-func CreateUserAndVillage(req dtos.RegisterRequestDTO) {
+	"github.com/KeshavBansal42/Lords_of_Larceny/backend/db"
+	"github.com/KeshavBansal42/Lords_of_Larceny/backend/dtos"
+)
 
+func CreateUserAndVillage(req dtos.RegisterRequestDTO) (int, error) {
+	ctx := context.Background()
+
+	tx, err := db.Conn.Begin(ctx)
+	if err != nil {
+		return 0, err
+	}
+	defer tx.Rollback(ctx)
+
+	var userID int
+	err = tx.QueryRow(ctx, "INSERT INTO users (username, password_hash) VALUES ($1, $2) RETURNING id", req.Username, req.PasswordHash).Scan(&userID)
+
+	if err != nil {
+		return 0, errors.New("username already exists")
+	}
+
+	_, err = tx.Exec(ctx, "INSERT INTO villages (user_id, town_hall_level, gold, elixir) VALUES ($1, 1, 1000, 1000)", userID)
+
+	if err != nil {
+
+		return 0, errors.New("failed to initialise village")
+	}
+
+	if err = tx.Commit(ctx); err != nil {
+		return 0, err
+	}
+
+	return userID, nil
 }
