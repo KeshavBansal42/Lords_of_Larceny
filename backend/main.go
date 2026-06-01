8390d797ad57d2fad41290fbc9e71bd42c716c98
Keshav Bansal <keshavbansal9a@gmail.com>
2026-06-02 02:14:09 +0530

    Completed one route

--- backend/main.go
+++ backend/main.go
@@ -1,13 +1,28 @@
 package main
 
 import (
+	"log"
+	"net/http"
+	"os"
+
 	"github.com/KeshavBansal42/Lords_of_Larceny/backend/db"
+	"github.com/KeshavBansal42/Lords_of_Larceny/backend/routes"
 	"github.com/gorilla/mux"
+	"github.com/joho/godotenv"
 )
 
 func main() {
 	db.InitDB()
 
 	r := mux.NewRouter()
+	routes.InitRoutes(r)
 
+	err := godotenv.Load()
+	if err != nil {
+		log.Println("Error loading .env file")
+	}
+
+	addrString := os.Getenv("SERVER_URI")
+
+	http.ListenAndServe(addrString, r)
 }
