b187f3141c2cf3a0b8ce4507a5d5b1f0efc03afe
Keshav Bansal <keshavbansal9a@gmail.com>
2026-06-01 20:35:31 +0530

    Added models

---
+++ backend/models/models.go
@@ -0,0 +1,50 @@
+package models
+
+import "time"
+
+type User struct {
+	ID           int       `json:"id"`
+	Username     string    `json:"username"`
+	PasswordHash string    `json:"-"`
+	CreatedAt    time.Time `json:"created_at"`
+}
+
+type Village struct {
+	ID            int `json:"id"`
+	UserID        int `json:"user_id"`
+	TownHallLevel int `json:"town_hall_level"`
+	Gold          int `json:"gold"`
+	Elixir        int `json:"elixir"`
+}
+
+type BuildingConfig struct {
+	ID        int    `json:"id"`
+	Name      string `json:"name"`
+	Level     int    `json:"level"`
+	HitPoints int    `json:"hit_points"`
+	Damage    int    `json:"damage"`
+	BuildCost int    `json:"build_cost"`
+}
+
+type TroopConfig struct {
+	ID           int    `json:"id"`
+	Name         string `json:"name"`
+	Level        int    `json:"level"`
+	HitPoints    int    `json:"hit_points"`
+	Damage       int    `json:"damage"`
+	TrainingCost int    `json:"training_cost"`
+}
+
+type VillageBuilding struct {
+	ID         int `json:"id"`
+	VillageID  int `json:"village_id"`
+	BuildingID int `json:"building_id"`
+	X          int `json:"x"`
+	Y          int `json:"y"`
+}
+
+type VillageTroop struct {
+	VillageID int `json:"village_id"`
+	TroopID   int `json:"troop_id"`
+	Quantity  int `json:"quantity"`
+}
