e68ce8544c2bdab480ba6cdc1b93fab6406cfa80
Keshav Bansal <keshavbansal9a@gmail.com>
2026-06-02 13:45:24 +0530

    Added controller for login

--- backend/controllers/controllers.go
+++ backend/controllers/controllers.go
@@ -3,9 +3,12 @@ package controllers
 import (
 	"encoding/json"
 	"net/http"
+	"os"
+	"time"
 
 	"github.com/KeshavBansal42/Lords_of_Larceny/backend/dtos"
 	"github.com/KeshavBansal42/Lords_of_Larceny/backend/repository"
+	"github.com/golang-jwt/jwt/v5"
 	"golang.org/x/crypto/bcrypt"
 )
 
@@ -17,22 +20,26 @@ func hashPassword(password string) (string, error) {
 func Register(w http.ResponseWriter, r *http.Request) {
 	if r.Method != http.MethodPost {
 		http.Error(w, "Method not allowed.", http.StatusMethodNotAllowed)
+		return
 	}
 
 	var req dtos.RegisterRequestDTO
 	err := json.NewDecoder(r.Body).Decode(&req)
 	if err != nil {
 		http.Error(w, "Invalid req body.", http.StatusBadRequest)
+		return
 	}
 
 	passwordHash, err := hashPassword(req.Password)
 	if err != nil {
 		http.Error(w, "Failed to hash password.", http.StatusInternalServerError)
+		return
 	}
 
 	userID, err := repository.CreateUserAndVillage(req.Username, passwordHash)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
 	}
 
 	res := dtos.RegisterResponseDTO{
@@ -41,5 +48,52 @@ func Register(w http.ResponseWriter, r *http.Request) {
 
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusCreated)
+	json.NewEncoder(w).Encode(res)
+}
+
+func Login(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		http.Error(w, "Method not allowed.", http.StatusMethodNotAllowed)
+		return
+	}
+
+	var req dtos.LoginRequestDTO
+	err := json.NewDecoder(r.Body).Decode(&req)
+	if err != nil {
+		http.Error(w, "Invalid req body.", http.StatusBadRequest)
+		return
+	}
+
+	jwtSecret := os.Getenv("JWT_SECRET")
+
+	userID, passwordHash, err := repository.GetUserByUsername(req.Username)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusUnauthorized)
+		return
+	}
+
+	err = bcrypt.CompareHashAndPassword([]byte(passwordHash), []byte(req.Password))
+	if err != nil {
+		http.Error(w, "Wrong credentials.", http.StatusUnauthorized)
+		return
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		"user_id": userID,
+		"exp":     time.Now().Add(24 * time.Hour).Unix(),
+	})
+
+	tokenString, err := token.SignedString([]byte(jwtSecret))
+	if err != nil {
+		http.Error(w, "Failed to generate token", http.StatusInternalServerError)
+		return
+	}
+
+	res := dtos.LoginResponseDTO{
+		Token: tokenString,
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
 	json.NewEncoder(w).Encode(res)
 }
