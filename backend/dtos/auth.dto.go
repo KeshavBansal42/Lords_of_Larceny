19f087daba0a74a626c8ff8f9b26cc8c3ddccd60
Keshav Bansal <keshavbansal9a@gmail.com>
2026-06-02 12:47:38 +0530

    Added DTOs for login

--- backend/dtos/auth.dto.go
+++ backend/dtos/auth.dto.go
@@ -8,3 +8,12 @@ type RegisterRequestDTO struct {
 type RegisterResponseDTO struct {
 	ID int `json:"id"`
 }
+
+type LoginRequestDTO struct {
+	Username string `json:"username"`
+	Password string `json:"password"`
+}
+
+type LoginResponseDTO struct {
+	Token string `json:"token"`
+}
