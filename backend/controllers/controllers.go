c765da5e978a0d7bfe85ae29dfbcb81a38cf2ce7
Keshav Bansal <keshavbansal9a@gmail.com>
2026-06-02 17:56:58 +0530

    Added controller function for finding village

--- backend/controllers/controllers.go
+++ backend/controllers/controllers.go
@@ -97,3 +97,35 @@ func Login(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK)
 	json.NewEncoder(w).Encode(res)
 }
+
+func GetVillage(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodGet {
+		http.Error(w, "Method not allowed.", http.StatusMethodNotAllowed)
+		return
+	}
+
+	value := r.Context().Value("userID")
+	userIDFloat, ok := value.(float64)
+	if !ok {
+		http.Error(w, "Invalid user ID in token", http.StatusInternalServerError)
+		return
+	}
+
+	userID := int(userIDFloat)
+
+	thlevel, gold, elixir, err := repository.GetVillageByUserID(userID)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	res := dtos.VillageResponseDTO{
+		TownHallLevel: thlevel,
+		Gold:          gold,
+		Elixir:        elixir,
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(res)
+}
