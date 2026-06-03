f0d14c1943f5f6af38c2a6d5a025eaa81738ff40
Keshav Bansal <keshavbansal9a@gmail.com>
2026-06-03 18:37:59 +0530

    Added controller for adding building

--- backend/controllers/controllers.go
+++ backend/controllers/controllers.go
@@ -167,3 +167,43 @@ func GetAllVillageBuildings(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK)
 	json.NewEncoder(w).Encode(res)
 }
+
+func AddBuilding(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		http.Error(w, "Method not allowed.", http.StatusMethodNotAllowed)
+		return
+	}
+
+	var req dtos.BuildRequestDTO
+	err := json.NewDecoder(r.Body).Decode(&req)
+	if err != nil {
+		http.Error(w, "Invalid req body.", http.StatusBadRequest)
+		return
+	}
+
+	value := r.Context().Value("userID")
+	userIDFloat, ok := value.(float64)
+	if !ok {
+		http.Error(w, "Invalid user ID in token", http.StatusInternalServerError)
+		return
+	}
+
+	userID := int(userIDFloat)
+
+	villageID, _, _, _, err := repository.GetVillageByUserID(userID)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	gold, err := repository.AddBuilding(villageID, req.BuildingID, req.X, req.Y)
+
+	res := dtos.BuildResponseDTO{
+		Message:       "Your building has been created.",
+		RemainingGold: gold,
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(res)
+}
