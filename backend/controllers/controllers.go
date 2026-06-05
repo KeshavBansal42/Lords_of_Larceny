d070780a1cc2c249cc018311b9e0463ffc5effd2
Keshav Bansal <keshavbansal9a@gmail.com>
2026-06-05 17:08:31 +0530

    Added controller for fetching village troops

--- backend/controllers/controllers.go
+++ backend/controllers/controllers.go
@@ -359,3 +359,39 @@ func TrainTroops(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK)
 	json.NewEncoder(w).Encode(res)
 }
+
+func GetAllVillageTroops(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodGet {
+		http.Error(w, "Method not allowed.", http.StatusMethodNotAllowed)
+		return
+	}
+
+	value := r.Context().Value("userID")
+	userIDFloat, ok := value.(float64)
+	if !ok {
+		http.Error(w, "Invalid user ID in token", http.StatusInternalServerError)
+		return
+	}
+
+	userID := int(userIDFloat)
+
+	villageID, _, _, _, err := repository.GetVillageByUserID(userID)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	troops, err := repository.GetAllVillageTroops(villageID)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	res := dtos.GetVillageTroopsResponseDTO{
+		Troops: troops,
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(res)
+}
