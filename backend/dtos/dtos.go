22fbd5e0f330937df5c5e34a607f28e4c754a9ed
Keshav Bansal <keshavbansal9a@gmail.com>
2026-06-05 16:58:41 +0530

    Added DTOs for fetching village troops

--- backend/dtos/dtos.go
+++ backend/dtos/dtos.go
@@ -83,3 +83,12 @@ type TrainTroopsRequestDTO struct {
 type TrainTroopsResponseDTO struct {
 	Message string `json:"message"`
 }
+
+type TroopResponseFromDBDTO struct {
+	TroopID  int `json:"troopid"`
+	Quantity int `json:"quantity"`
+}
+
+type GetVillageTroopsResponseDTO struct {
+	Troops []TroopResponseFromDBDTO `json:"troops"`
+}
