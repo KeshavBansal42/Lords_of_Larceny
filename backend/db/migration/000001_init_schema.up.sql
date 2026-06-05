df261b5525042414463fe1fc46b942252935a353
Keshav Bansal <keshavbansal9a@gmail.com>
2026-06-05 13:30:02 +0530

    Updated schema for resource types

--- backend/db/migration/000001_init_schema.up.sql
+++ backend/db/migration/000001_init_schema.up.sql
@@ -21,6 +21,7 @@ CREATE TABLE building_configs (
     hit_points INT NOT NULL,
     damage INT NOT NULL DEFAULT 0,
     build_cost INT NOT NULL,
+    build_resource_type VARCHAR(20) NOT NULL DEFAULT 'gold',
     production_per_min INT NOT NULL DEFAULT 0,
     capacity INT NOT NULL DEFAULT 0,
     size INT NOT NULL DEFAULT 2,
