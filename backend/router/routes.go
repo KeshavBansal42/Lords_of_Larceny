3c6de68addf4bbde5543664686cf5eb59148298e
Keshav Bansal <keshavbansal9a@gmail.com>
2026-06-05 17:10:57 +0530

    Updated routes

--- backend/router/routes.go
+++ backend/router/routes.go
@@ -13,11 +13,12 @@ func InitRoutes() *mux.Router {
 	router.HandleFunc("/login", controllers.Login).Methods("POST")
 	router.HandleFunc("/village", middleware.RequireAuth(controllers.GetVillage)).Methods("GET")
 	router.HandleFunc("/village/buildings", middleware.RequireAuth(controllers.GetAllVillageBuildings)).Methods("GET")
-	router.HandleFunc("/village/build", middleware.RequireAuth(controllers.AddBuilding)).Methods("POST")
-	router.HandleFunc("/village/collect", middleware.RequireAuth(controllers.CollectResources)).Methods("PUT")
-	router.HandleFunc("/village/upgrade", middleware.RequireAuth(controllers.UpgradeBuilding)).Methods("PUT")
-	router.HandleFunc("/village/move", middleware.RequireAuth(controllers.MoveBuilding)).Methods("PUT")
+	router.HandleFunc("/village/buildings/build", middleware.RequireAuth(controllers.AddBuilding)).Methods("POST")
+	router.HandleFunc("/village/buildings/upgrade", middleware.RequireAuth(controllers.UpgradeBuilding)).Methods("PUT")
+	router.HandleFunc("/village/buildings/move", middleware.RequireAuth(controllers.MoveBuilding)).Methods("PUT")
+	router.HandleFunc("/village/troops", middleware.RequireAuth(controllers.GetAllVillageTroops)).Methods("GET")
 	router.HandleFunc("/village/troops/train", middleware.RequireAuth(controllers.TrainTroops)).Methods("PUT")
+	router.HandleFunc("/village/resources/collect", middleware.RequireAuth(controllers.CollectResources)).Methods("PUT")
 
 	return router
 }
